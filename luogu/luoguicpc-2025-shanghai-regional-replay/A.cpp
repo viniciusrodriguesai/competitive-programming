edcd918f5375ec317a167f2e57612969b956d232
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-07 17:30:28 -0300

    refactor: clarify problem A tree variable names

--- luogu/luoguicpc-2025-shanghai-regional-replay/A.cpp
+++ luogu/luoguicpc-2025-shanghai-regional-replay/A.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 int main(){
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int T;
-    cin>>T;
-    while(T--){
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
         int n;
         cin>>n;
-        vector<int> p(n+1),dep(n+1),tin(n+1),tout(n+1),cnt(n+1);
-        vector<vector<int>> g(n+1);
-        int mx=0;
+        vector<int> pai(n+1),nivel(n+1);
+        vector<vector<int>> filhos(n+1);
+        int maior=0;
         for(int i=2;i<=n;i++){
-            cin>>p[i];
-            g[p[i]].push_back(i);
-            dep[i]=dep[p[i]]+1;
-            mx=max(mx,dep[i]);
+            cin>>pai[i];
+            filhos[pai[i]].push_back(i);
+            nivel[i]=nivel[pai[i]]+1;
+            maior=max(maior,nivel[i]);
         }
