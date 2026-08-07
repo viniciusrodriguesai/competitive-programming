a31970f23404bfb74be0c6b42feb0280ce58f0f3
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-07 17:31:56 -0300

    feat: add interactive depth search for problem A

--- luogu/luoguicpc-2025-shanghai-regional-replay/A.cpp
+++ luogu/luoguicpc-2025-shanghai-regional-replay/A.cpp
@@ -17,3 +17,25 @@ int main(){
             nivel[i]=nivel[pai[i]]+1;
             maior=max(maior,nivel[i]);
         }
+        auto pergunta=[&](int u,int k){
+            cout<<"? "<<u<<" "<<k<<endl;
+            int x;
+            cin>>x;
+            if(x==-1) exit(0);
+            return x;
+        };
+        int l=-1,r=maior;
+        while(r-l>1){
+            int m=(l+r)/2;
+            if(pergunta(1,m)) r=m;
+            else l=m;
+        }
+        int d=r;
+        vector<int> tin(n+1),tout(n+1);
+        int tempo=0;
+        function<void(int)> dfs=[&](int u){
+            tin[u]=++tempo;
+            for(int v:filhos[u]) dfs(v);
+            tout[u]=tempo;
+        };
+        dfs(1);
