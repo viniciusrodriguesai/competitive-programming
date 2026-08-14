3028d376c5b034a59f57c337c0978a29f738591c
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-14 09:56:49 -0300

    Sort values for target sum search

--- Pacode/Final_Prove/C.cpp
+++ Pacode/Final_Prove/C.cpp
@@ -5,9 +5,10 @@ int main() {
     long long x;
     cin >> n >> x;
     vector<pair<long long, int>> v;
-    for (int i=0;i<n;i++) {
+    for (int i = 0; i < n; i++) {
         long long valor;
         cin >> valor;
         v.push_back({valor, i + 1});
     }
+    sort(v.begin(), v.end());
 }
