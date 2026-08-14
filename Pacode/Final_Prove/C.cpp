fd4dad3e3ba92693165f0968c530cf5baf3415b4
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-14 09:53:00 -0300

    Rename C solution and read array values

---
+++ Pacode/Final_Prove/C.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    long long x;
+    cin >> n >> x;
+    vector<pair<long long, int>> v;
+    for (int i=0;i<n;i++) {
+        long long valor;
+        cin >> valor;
+        v.push_back({valor, i + 1});
+    }
+}
