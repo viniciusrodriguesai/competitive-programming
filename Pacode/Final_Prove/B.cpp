8c4b7810884c027a1d32db371e8ae3e80bba4e11
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-14 09:34:21 -0300

    Add binary search loop for queries

--- Pacode/Final_Prove/B.cpp
+++ Pacode/Final_Prove/B.cpp
@@ -1,19 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
     int n, q;
     cin >> n >> q;
+
     vector<int> v;
+
     for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
         v.push_back(x);
     }
-    for(int i = 0; i<q;i++) {
+
+    for (int i = 0; i < q; i++) {
         int x;
         cin >> x;
+
         int l = 0;
         int r = n - 1;
         int ans = -1;
+
+        while (l <= r) {
+            }
+        }
     }
 }
