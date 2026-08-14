b5360cf89b9769bd9b512b622154171efa64912b
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-14 09:18:29 -0300

    Sort and print input values

--- Pacode/Final_Prove/A.cpp
+++ Pacode/Final_Prove/A.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n;
-cin >> n;
-for (int i = 0; i < n; i++) {
-    int x;
-    cin >> x;
-    v.push_back(x);
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i=0;i< n;i++){
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    sort(v.begin(), v.end());
+    for(int i=0;i<n;i++){
+        cout << v[i] << " ";
+    }
 }
