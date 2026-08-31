8504760b61ed063fd25e1ce89bf45531c9a2e0ae
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-31 16:52:40 -0300

    docs: add leveling activity prompt

---
+++ study/basic-cpp/atividade-de-nivelamento.cpp
@@ -0,0 +1,19 @@
+/*
+ * Atividade de Nivelamento
+ *
+ * Faça um programa que:
+ * - leia um número inteiro N, que representa a quantidade de números;
+ * - leia N números inteiros;
+ * - calcule e imprima a soma apenas dos números pares.
+ *
+ * Entrada de teste:
+ * 5
+ * 2 7 3 8 10
+ *
+ * Saída esperada:
+ * 20
+ *
+ * Explicação: os números pares são 2, 8 e 10.
+ * A soma é 2 + 8 + 10 = 20.
+ */
+
