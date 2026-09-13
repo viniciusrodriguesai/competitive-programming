9f6ec73f835dde998c7f989eba7b2104a796b007
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-09-13 19:59:11 -0300

    Prepara arquivo C++ da questão C sem solução / Add problem C C++ starter without solution

---
+++ Pacode/2026.2/turma iniciante/Lista Iniciante - STL (Vector - Pair - Stack - Queue)/C - Cards/C-cards.cpp
@@ -0,0 +1,46 @@
+/*
+ * Problem C - Cards
+ * Limite de tempo: 2000 ms
+ * Limite de memoria: 262144 kB
+ *
+ * Serezha ganhou cartas com letras. Elas formavam palavras que representavam
+ * o numero favorito de sua mae em notacao binaria, mas ele embaralhou as cartas.
+ * Reorganize as letras para recuperar o maior numero possivel.
+ *
+ * As palavras possiveis sao "zero", que representa o digito 0, e "one",
+ * que representa o digito 1. E garantido que as letras podem ser
+ * reorganizadas em uma sequencia dessas palavras.
+ *
+ * Entrada:
+ * - Primeira linha: um inteiro n, o tamanho da string.
+ * - Segunda linha: uma string com letras minusculas entre 'z', 'e', 'r', 'o' e 'n'.
+ *
+ * Restricoes:
+ * - 1 <= n <= 100000
+ *
+ * Saida:
+ * - Imprima o maior numero possivel em notacao binaria.
+ * - Separe os digitos por espacos. Zeros a esquerda sao permitidos.
+ *
+ * Exemplo 1:
+ * Entrada:
+ * 4
+ * ezor
+ * Saida:
+ * 0
+ *
+ * Exemplo 2:
+ * Entrada:
+ * 10
+ * nznooeeoer
+ * Saida:
+ * 1 1 0
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long
+    return 0;
+}
