8ed2c7262a32742d89d0d226e359371d45ed9f39
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-09-13 19:58:38 -0300

    Registra início da questão B sem solução / Save unfinished work on problem B

---
+++ Pacode/2026.2/turma iniciante/Lista Iniciante - STL (Vector - Pair - Stack - Queue)/B - Good Sequence/B-good-sequence.cpp
@@ -0,0 +1,84 @@
+/*
+ * Problem B - Good Sequence
+ * Limite de tempo: 2000 ms
+ *
+ * Voce recebe uma sequencia de N numeros inteiros positivos:
+ * a = (a1, a2, ..., aN).
+ *
+ * Remova alguns elementos para que a sequencia restante seja boa.
+ * Uma sequencia e boa quando, para cada valor x presente nela,
+ * x aparece exatamente x vezes.
+ *
+ * Exemplos de sequencias boas:
+ * (3, 3, 3), (4, 2, 4, 1, 4, 2, 4) e a sequencia vazia.
+ * Exemplos que nao sao boas:
+ * (3, 3, 3, 3) e (2, 4, 1, 4, 2).
+ *
+ * Encontre o numero minimo de elementos que precisam ser removidos.
+ *
+ * Entrada:
+ * - Primeira linha: N.
+ * - Segunda linha: N inteiros a1, a2, ..., aN.
+ *
+ * Restricoes:
+ * - 1 <= N <= 100000
+ * - 1 <= ai <= 1000000000
+ *
+ * Saida:
+ * - Imprima o numero minimo de remocoes para formar uma sequencia boa.
+ *
+ * Exemplo 1:
+ * Entrada:
+ * 4
+ * 3 3 3 3
+ * Saida:
+ * 1
+ *
+ * Exemplo 2:
+ * Entrada:
+ * 5
+ * 2 4 1 4 2
+ * Saida:
+ * 2
+ *
+ * Exemplo 3:
+ * Entrada:
+ * 6
+ * 1 2 2 3 3 3
+ * Saida:
+ * 0
+ *
+ * Exemplo 4:
+ * Entrada:
+ * 1
+ * 1000000000
+ * Saida:
+ * 1
+ *
+ * Exemplo 5:
+ * Entrada:
+ * 8
+ * 2 7 1 8 2 8 1 8
+ * Saida:
+ * 5
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long Numeros, Numero;
+
+    cin >> Numeros, Numero;
+
+    long long contador = 0;
+
+    vector <int> vetor;
+
+    for(int i = 0 ; i < Numero; i ++ ){
+
+        vetor[i]
+
+    }
+    return 0;
+}
