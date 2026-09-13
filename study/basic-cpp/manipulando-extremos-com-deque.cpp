47ee88d853acd2d2e8a65589c8ec0042a970133a
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-09-13 17:52:30 -0300

    Add deque extremes exercise

---
+++ study/basic-cpp/manipulando-extremos-com-deque.cpp
@@ -0,0 +1,32 @@
+/*
+ * ATIVIDADE OBRIGATORIA
+ *
+ * Para cada numero lido:
+ * - se for par, insira no inicio do deque;
+ * - se for impar, insira no final do deque.
+ * Ao final, imprima o deque do inicio ao fim.
+ *
+ * Entrada de teste:
+ * 4
+ * 1 2 3 4
+ *
+ * Saida esperada:
+ * 4 2 1 3
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    long long numero,par,impar;
+
+    cin >> numero;
+
+
+    deque<int> dq;
+
+    if(numero %)
+
+    return 0;
+}
