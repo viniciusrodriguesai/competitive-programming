8b3db571fbd399c660d82a95688708a5d81cf859
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-09-13 18:15:22 -0300

    Adiciona exercício Vector-Sort da turma iniciante
    Cria A-vector-sort.cpp com o enunciado, as instruções de entrada e saída, um exemplo e o esqueleto em C++ para desenvolver a solução.

---
+++ Pacode/2026.2/turma iniciante/Lista Iniciante - STL (Vector - Pair - Stack - Queue)/A-vector-sort.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem A - Vector-Sort
+ *
+ * Voce recebeu N numeros inteiros. Ordene esses numeros e imprima-os
+ * em ordem crescente.
+ *
+ * Para armazenar os numeros, use um vector<int>. Vectors sao containers
+ * sequenciais que funcionam como vetores de tamanho variavel.
+ *
+ * Entrada:
+ * - A primeira linha contem N, a quantidade de numeros inteiros.
+ * - A segunda linha contem os N numeros inteiros.
+ *
+ * Saida:
+ * - Imprima os numeros em ordem crescente, todos na mesma linha,
+ *   separados por espaco.
+ *
+ * Exemplo de entrada:
+ * 5
+ * 1 6 10 8 4
+ *
+ * Exemplo de saida:
+ * 1 4 6 8 10
+ *
+ * Dica: vector<int> v; e sort(v.begin(), v.end());
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Escreva sua solucao aqui.
+    return 0;
+}
