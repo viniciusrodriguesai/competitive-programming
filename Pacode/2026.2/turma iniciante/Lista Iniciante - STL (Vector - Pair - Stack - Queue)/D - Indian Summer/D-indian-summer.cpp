0bad22fad530fd51601f48f3bc6b425c7e23d825
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-09-16 14:46:50 -0300

    chore(stl): add Indian Summer starter file

---
+++ Pacode/2026.2/turma iniciante/Lista Iniciante - STL (Vector - Pair - Stack - Queue)/D - Indian Summer/D-indian-summer.cpp
@@ -0,0 +1,39 @@
+/*
+ * Problem D - Indian Summer
+ * Limite de tempo: 2000 ms
+ * Limite de memoria: 262144 kB
+ *
+ * Alyona esta recolhendo folhas caidas na floresta. Ela escolhe uma folha
+ * somente quando ainda nao possui outra com a mesma especie de arvore e a
+ * mesma cor.
+ *
+ * Entrada:
+ * - A primeira linha contem um inteiro n, a quantidade de folhas encontradas.
+ * - As proximas n linhas contem a especie da arvore e a cor de cada folha.
+ *
+ * Restricoes:
+ * - 1 <= n <= 100
+ * - Cada nome possui no maximo 10 letras minusculas.
+ *
+ * Saida:
+ * - Imprima a quantidade de folhas escolhidas por Alyona.
+ *
+ * Exemplo:
+ * Entrada:
+ * 5
+ * birch yellow
+ * maple red
+ * birch yellow
+ * maple yellow
+ * maple green
+ *
+ * Saida:
+ * 4
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    return 0;
+}
