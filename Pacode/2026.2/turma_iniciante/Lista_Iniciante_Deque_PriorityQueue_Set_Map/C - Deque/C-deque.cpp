63d20c1c6ab94d8b3465ab87c630d0fb18a1af28
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-09-20 11:16:28 -0300

    wip(stl): start deque query parsing

---
+++ Pacode/2026.2/turma_iniciante/Lista_Iniciante_Deque_PriorityQueue_Set_Map/C - Deque/C-deque.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int quantidade;
+    cin >> quantidade;
+    deque <int> numeros;
+    for(int i = 0; i < quantidade; i++){
+        int operacao,int posicao, int numero; 
+        cin >> operacao >> posicao >> numero;
+    }
+
+
+
+    return 0;
+}
