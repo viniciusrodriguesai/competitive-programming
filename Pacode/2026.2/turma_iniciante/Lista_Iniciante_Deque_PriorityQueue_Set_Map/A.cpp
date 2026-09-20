f7da416d3f87a130f0efd3f0dd2bf51de45550c1
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-09-19 21:27:36 -0300

    wip(stl): start problem A solution
    Add the current work-in-progress implementation for the first priority queue exercise. The solution is intentionally incomplete and preserves the code as currently written.

---
+++ Pacode/2026.2/turma_iniciante/Lista_Iniciante_Deque_PriorityQueue_Set_Map/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int numero;
+    string comando;
+    priority_queue<int> pq;
+    cin >> comando;
+    while(comando != "end" ){
+            if(comando == "insert"){
+                        cin >> comando;
+
+            }
+    }
+    return 0;
+}
